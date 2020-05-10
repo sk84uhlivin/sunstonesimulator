fe7044724c541e46ea59a4cedcd835f905ddd4ca
sk84uhlivin <sk84uhlivin@gmail.com>
2020-05-10 18:40:00 -0400

    Full working version, cleaned up.

--- data.py
+++ data.py
@@ -1,14 +1,16 @@
 import random
 import plotly.graph_objects as go
 
+# Globals.
 two_c = 0
 three_c = 0
 four_c = 0
-sims = 30000
+
+# Constants.
 x_axis = ["2 Crystal", "3 Crystal", "4 Crystal"]
-print()
 
 
+# Function rolls 4 crystals.
 def rolls():
     roll1 = random.randint(25, 65)
     roll2 = random.randint(25, 65)
@@ -17,40 +19,54 @@ def rolls():
     return roll1, roll2, roll3, roll4
 
 
-for x in range(1, sims + 1):
-    a, b, c, d = rolls()
+while True:
+    # User defined simulation count.
+    print()
+    try:
+        sims = int(input("How many simulations? "))
+    except ValueError:
+        print()
+        print('Invalid command.')
+        continue
 
-    # 2 Crystal
-    if a + b >= 100:
-        two_c += 1
+    # Counts results.
+    for x in range(1, sims + 1):
+        a, b, c, d = rolls()
 
-    # 3 Crystal
-    elif a + b + c >= 100:
-        three_c += 1
+        # 2 Crystal
+        if a + b >= 100:
+            two_c += 1
 
-    # 4 Crystal
-    else:
-        four_c += 1
+        # 3 Crystal
+        elif a + b + c >= 100:
+            three_c += 1
 
-print("Generating media...")
-# layout = go.Layout(legend={'traceorder': 'normal'}
-# fig = go.Figure(data=[go.Pie(labels=x_axis, values=[two_c, three_c, four_c])])
-fig = go.Figure()
-fig.add_trace(go.Pie(labels=x_axis, values=[two_c, three_c, four_c], sort=False))
-fig.update_layout(
-    title=f"Sunstone Simulation - {sims} times",
+        # 4 Crystal
+        else:
+            four_c += 1
 
-    font=dict(
-        family="Tahoma, sans-serif",
-        size=18,
-        color="#000000"
-    ),
+    # Generates pie chart.
+    print()
+    print("Generating chart...")
+    fig = go.Figure()
+    fig.add_trace(go.Pie(labels=x_axis, values=[two_c, three_c, four_c], sort=False))
+    fig.update_layout(
+        title=f"Sunstone Simulation - {sims:,} times",
+        font=dict(
+            family="Tahoma, sans-serif",
+            size=18,
+            color="#000000"
+        ),
+    )
 
-)
-fig.show()
+    # Pushes chart.
+    fig.show()
 
-print(f"2 Crystals: {two_c}")
-print(f"3 Crystals: {three_c}")
-print(f"4 Crystals: {four_c}")
-
-print(f"{sims} simulations.")
+    # Prints results to console.
+    print()
+    print(f"2 Crystals: {two_c:,}")
+    print(f"3 Crystals: {three_c:,}")
+    print(f"4 Crystals: {four_c:,}")
+    print()
+    print(f"Done over {sims:,} simulations.")
+    break
